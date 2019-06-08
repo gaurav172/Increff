9e85909dc78e86aac8fe63b1cdc8ab96f44e4c3f
vivace_jr777 <arnav.juneja@students.iiit.ac.in>
2019-06-09 01:13:12 +0530

    minor

--- tutorial/flaskr/buyer.py
+++ tutorial/flaskr/buyer.py
@@ -81,33 +81,6 @@ def menu(uname):
     return render_template("buyer/menu.html")
 
 
-@bp.route("/sell",methods=("GET", "POST"))
-def sell():
-    db = get_db()
-    g.dishList = ( db.execute("SELECT * FROM item WHERE sellerUsername = ?", (g.user["username"],)).fetchall() )
-    if request.method == "POST" :
-        dishes = request.form.getlist('name')
-        quantity = request.form["quantity"]
-        stime = request.form["stime"]
-        etime = request.form["etime"]
-        print(dishes)
-        for x in dishes:       
-            print(x)
-            price= ( db.execute("SELECT price FROM item WHERE sellerUsername = ? and name = ? ", (g.user["username"],x)).fetchone() )
-            description= ( db.execute("SELECT description FROM item WHERE sellerUsername = ? and name = ? ", (g.user["username"],x)).fetchone() )
-            typ= ( db.execute("SELECT type FROM item WHERE sellerUsername = ? and name = ? ", (g.user["username"],x)).fetchone() )
-            price=price[0]
-            description=description[0]
-            typ=typ[0]
-            db.execute(
-                "INSERT INTO sell (name,sellerUsername,price,qAvail,readyTime,sellingTill,description,type) VALUES (?,?,?,?,?,?,?,?)",
-                (x,g.user['username'],price,quantity,etime,stime,description,typ)
-            )
-            db.commit()
-        return redirect(url_for("dish.sell"))
-    return render_template("dish/sell.html")  
-
-
 @bp.route("/meal<id>")
 def meal(id):
     db = get_db()
